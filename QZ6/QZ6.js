b194a1464f34ad6e6abd41c228669edde43d3285
yuan17555 <100269246+yuan17555@users.noreply.github.com>
2022-04-27 15:30:53 +0800

    Update QZ6.js

--- QZ6/QZ6.js
+++ QZ6/QZ6.js
@@ -1,8 +1,7 @@
-setInterval(myFunction, 1000)
-function myFunction() {
+setInterval(function myFunction() {     
   var now = new Date();
   //var hh = now.getHours();
   //var mm = now.getMinutes();
   //var ss = now.getSeconds();
   document.getElementById('clock').innerHTML = now.getHours() + ":" + now.getMinutes() + ":" +now.getSeconds();
-}
+}, 1000);
